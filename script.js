2f46ec4d12323e081bc871776e1e5d7b68b2ca2a
robitu <robertoiturralde2009@hotmail.com>
2024-05-25 19:07:11 -0600

    QR code reader

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-import React from "react";
-import Webcam from "react-webcam";
-
-const videoConstraints = {
-  facingMode: { exact: "environment" }, // for rear camera, use "user" for front camera
-};
+import React, { useEffect } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 const MyComponent = () => {
-  return (
-    <Webcam
-      audio={false}
-      height={720}
-      screenshotFormat="image/jpeg"
-      width={1280}
-      videoConstraints={videoConstraints}
-    />
-  );
+  useEffect(() => {
+    const html5QrCode = new Html5QrcodeScanner(
+      "reader",
+      { fps: 10, qrbox: { width: 250, height: 250 } },
+      false
+    );
+    html5QrCode.render(onSuccess, onError);
+
+    function onSuccess(decodedText, decodedResult) {
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+    }
+
+    function onError(errorMessage) {
+      console.log(`Error scanning = ${errorMessage}`);
+    }
+
+    return () => {
+      html5QrCode.clear().catch((error) => {
+        console.error("Failed to clear html5QrcodeScanner.", error);
+      });
+    };
+  }, []);
+
+  return <div id="reader"></div>;
 };
 
 export default MyComponent;
